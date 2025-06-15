552727c97859ceb161bfedad641f97bf5bef307f
Radosław Mocarski <mocarski@student.agh.edu.pl>
2025-06-15 19:40:11 +0200

    Dodanie wielokrotnego liczenia fitnesu do genu

--- Master/src/Trainer/UnityCommunication.cs
+++ Master/src/Trainer/UnityCommunication.cs
@@ -22,6 +22,7 @@ namespace Trainer
 		public static readonly string WORKER_PATH = Path.Combine(AppContext.BaseDirectory, @"..", "..", "..", "..", $"Workers/Worker_");
 		public static readonly int MAX_GENOMES_PER_WORKER = 10; // Maksymalna liczba genomów na jednego workera, może być dostosowana
 		public static readonly bool CAMERA = true; // Czy kamera ma być aktywna w symulacji Unity
+		public static readonly int NUM_REPETITIONS_SIMULATIONS = 3; // Liczba powtórzeń symulacji dla każdego genomu, może być dostosowana
 		#endregion Fields
 
 		#region Constructors
@@ -37,7 +38,7 @@ namespace Trainer
 		{
 			string workerDir = $"{WORKER_PATH}{workerId}";
 			Directory.CreateDirectory(workerDir);
-			for(int i = 0; i < genomes.Count; i++)
+			for (int i = 0; i < genomes.Count; i++)
 			{
 				NeatGenome genome = genomes[i];
 				string filePath = Path.Combine(workerDir, $"genome{i}.xml");
@@ -100,8 +101,9 @@ namespace Trainer
 		public void RunSimulations(ICollection<NeatGenome> genomes)
 		{
 			LastBestFitness = 0;
-			int checkingWorker = 0;
+			Dictionary<uint, double> fitnessesSum = new Dictionary<uint, double>();
 
+			int checkingWorker = 0;
 			Dictionary<int, List<uint>> activeThreads = new(); // Key-no. worker, value-genome id
 			HashSet<uint> genomeValidator = new HashSet<uint>();
 			foreach (NeatGenome gen in genomes)
@@ -109,64 +111,70 @@ namespace Trainer
 				if (genomeValidator.Contains(gen.Id))
 					Console.WriteLine("POWTARZA SIE ID !!!!!!!!!!!!!!!! !!!!!!!!!!!!!!!! !!!!!!!!!!!!!!!! !!!!!!!!!!!!!!!! !!!!!!!!!!!!!!!!");
 				else
+				{
 					genomeValidator.Add(gen.Id);
+					fitnessesSum.Add(gen.Id, 0); // Inicjalizuj sumę fitnessów dla każdego genomu
+				}
 			}
-
-			Dictionary<uint, NeatGenome> genomeDict = genomes.ToDictionary(g => g.Id); //key-genome id, value-genome
-			Queue<NeatGenome> genomesToCalculateFitness = new(genomes); //[.. genomes];  genomes.ToList();
-			int seed = new Random().Next(0, 1000000); // Losowy seed dla symulacji, może być przekazany do Unity
-			int turn = new Random().Next(2800, 4800);
-
-			while (genomeDict.Count > 0)
+			for (int rep = 0; rep < NUM_REPETITIONS_SIMULATIONS; rep++)
 			{
-				if (activeThreads.ContainsKey(checkingWorker))
+				Dictionary<uint, NeatGenome> genomeDict = genomes.ToDictionary(g => g.Id); //key-genome id, value-genome
+				Queue<NeatGenome> genomesToCalculateFitness = new(genomes); //[.. genomes];  genomes.ToList();
+				int seed = new Random().Next(0, 1000000); // Losowy seed dla symulacji, może być przekazany do Unity
+				int turn = new Random().Next(2800, 4800);
+				while (genomeDict.Count > 0)
 				{
-					Dictionary<uint,double>? fitnesses = GetFitnesses(checkingWorker);
-					if (!Object.Equals(fitnesses, null))
+					if (activeThreads.ContainsKey(checkingWorker))
 					{
-						File.Delete($"{WORKER_PATH}{checkingWorker}/result.json");
-						foreach (var fitness in fitnesses)
+						Dictionary<uint, double>? fitnesses = GetFitnesses(checkingWorker);
+						if (!Object.Equals(fitnesses, null))
 						{
-							LastBestFitness = Math.Max(LastBestFitness, fitness.Value);
-							foreach(uint genome_id in activeThreads[checkingWorker])
+							File.Delete($"{WORKER_PATH}{checkingWorker}/result.json");
+							foreach (var fitness in fitnesses)
 							{
-								genomeDict[genome_id].EvaluationInfo.SetFitness(fitness.Value); //aktualizuj fitness
-								Console.WriteLine($"Worker {checkingWorker} finished processing genome {genome_id} with fitness {fitness.Value}");
-								genomeDict.Remove(genome_id);
+								fitnessesSum[fitness.Key] += fitness.Value; // Sumuj fitnessy dla każdego genomu
+								//genomeDict[fitness.Key].EvaluationInfo.SetFitness(fitness.Value); //aktualizuj fitness
+								Console.WriteLine($"Worker {checkingWorker} finished processing genome {fitness.Key} with fitness {fitness.Value}");
+								genomeDict.Remove(fitness.Key);
 							}
+							activeThreads.Remove(checkingWorker);
 						}
-						activeThreads.Remove(checkingWorker);
 					}
-				}
 
-				if ((!activeThreads.ContainsKey(checkingWorker)) && genomesToCalculateFitness.Count > 0)
-				{
-					int genomesCount = Math.Min(MAX_GENOMES_PER_WORKER, genomesToCalculateFitness.Count);
-					List<NeatGenome> genomesToSend = new();
-					List<uint> genomeIdsToSend = new();
-					for (int i = 0; i < genomesCount; i++)
+					if ((!activeThreads.ContainsKey(checkingWorker)) && genomesToCalculateFitness.Count > 0)
 					{
-						NeatGenome nextGenome = genomesToCalculateFitness.Dequeue();
-						genomesToSend.Add(nextGenome);
-						genomeIdsToSend.Add(nextGenome.Id);
+						int genomesCount = Math.Min(MAX_GENOMES_PER_WORKER, genomesToCalculateFitness.Count);
+						List<NeatGenome> genomesToSend = new();
+						List<uint> genomeIdsToSend = new();
+						for (int i = 0; i < genomesCount; i++)
+						{
+							NeatGenome nextGenome = genomesToCalculateFitness.Dequeue();
+							genomesToSend.Add(nextGenome);
+							genomeIdsToSend.Add(nextGenome.Id);
+						}
+						activeThreads.Add(checkingWorker, genomeIdsToSend);
+						SaveGenomes(genomesToSend, checkingWorker);
+						var psi = new ProcessStartInfo
+						{
+							FileName = UNITY_PATH,
+							//Arguments = $"-batchmode -nographics -executeMethod SimRunner.Run -workerId {checkingWorker} -seedNo {seed} -logFile log_{checkingWorker}_{DateTime.Now.Month}_{DateTime.Now.Day}_{DateTime.Now.Hour}_{DateTime.Now.Minute}.txt -camera {Convert.ToInt32(CAMERA)} -genomesCount {genomesCount}",
+							//Arguments = $"-executeMethod SimRunner.Run -workerId {checkingWorker} -turnsNo {turn} -seedNo {seed} -logFile log_{checkingWorker}.txt -screen-width 800 -screen-height 600 -window-mode borderless -camera {Convert.ToInt32(CAMERA)} -genomesCount {genomesCount}",
+							Arguments = $"-executeMethod SimRunner.Run -workerId {checkingWorker} -turnsNo {turn} -seedNo {seed} -logFile log_{checkingWorker}.txt -screen-width 800 -screen-height 600 -window-mode windowed -camera {Convert.ToInt32(CAMERA)} -genomesCount {genomesCount}",
+							WorkingDirectory = $"{WORKER_PATH}{checkingWorker}",
+							UseShellExecute = false
+						};
+
+						Process.Start(psi);
 					}
-					activeThreads.Add(checkingWorker, genomeIdsToSend);
-					SaveGenomes(genomesToSend, checkingWorker);
-					var psi = new ProcessStartInfo
-					{
-						FileName = UNITY_PATH,
-						//Arguments = $"-batchmode -nographics -executeMethod SimRunner.Run -workerId {checkingWorker} -seedNo {seed} -logFile log_{checkingWorker}_{DateTime.Now.Month}_{DateTime.Now.Day}_{DateTime.Now.Hour}_{DateTime.Now.Minute}.txt -camera {Convert.ToInt32(CAMERA)} -genomesCount {genomesCount}",
-						//Arguments = $"-executeMethod SimRunner.Run -workerId {checkingWorker} -turnsNo {turn} -seedNo {seed} -logFile log_{checkingWorker}.txt -screen-width 800 -screen-height 600 -window-mode borderless -camera {Convert.ToInt32(CAMERA)} -genomesCount {genomesCount}",
-						Arguments = $"-executeMethod SimRunner.Run -workerId {checkingWorker} -turnsNo {turn} -seedNo {seed} -logFile log_{checkingWorker}.txt -screen-width 800 -screen-height 600 -window-mode windowed -camera {Convert.ToInt32(CAMERA)} -genomesCount {genomesCount}",
-						WorkingDirectory = $"{WORKER_PATH}{checkingWorker}",
-						UseShellExecute = false
-					};
-
-					Process.Start(psi);
+					checkingWorker = (checkingWorker + 1) % unityThreads;
+					Thread.Sleep(25);
 				}
-
-				checkingWorker = (checkingWorker + 1) % unityThreads;
-				Thread.Sleep(25);
+			}
+			foreach(var gen in genomes)
+			{
+				gen.EvaluationInfo.SetFitness(fitnessesSum[gen.Id]);
+				Console.WriteLine($"Genom ID: {gen.Id} -> Suma fitnessów: {fitnessesSum[gen.Id]}");
+				LastBestFitness = Math.Max(LastBestFitness, fitnessesSum[gen.Id]);
 			}
 		}
 		#endregion Public Methods
